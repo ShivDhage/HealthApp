c28025327f7bf75b888dc04d7b2b62b49d8ac8af
mukundm2 <mmukund@gmail.com>
2018-12-04 16:51:10 -0600

    first commit

--- app/src/main/java/com/example/shiv/healthapp/MainActivity.java
+++ app/src/main/java/com/example/shiv/healthapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.shiv.healthapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.Activity_dropdown);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
     }
 }
