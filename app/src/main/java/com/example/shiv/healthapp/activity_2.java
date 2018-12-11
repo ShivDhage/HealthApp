c42b7e707f4358b1306611504c05fc907f474158
Shiv Dhage <sdhage2@illinois.edu>
2018-12-11 01:48:06 -0600

    added second and third activity

---
+++ app/src/main/java/com/example/shiv/healthapp/activity_2.java
@@ -0,0 +1,13 @@
+package com.example.shiv.healthapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
