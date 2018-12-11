bbbdf1090e661f834aba159df29868c719e27168
Shiv Dhage <sdhage2@illinois.edu>
2018-12-11 16:40:34 -0600

    added ui to 3rd activity

--- app/src/main/java/com/example/shiv/healthapp/activity_2.java
+++ app/src/main/java/com/example/shiv/healthapp/activity_2.java
@@ -2,15 +2,33 @@ package com.example.shiv.healthapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.ToggleButton;
 
 
 public class activity_2 extends AppCompatActivity {
+
+    private static final String LOG_TAG = "Button";
+
+    String loseOrGain = "";
+    int pounds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        final ToggleButton loseGainButton = findViewById(R.id.loseGainButton);
+        loseGainButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                loseOrGain = "gain";
+                Log.d(LOG_TAG, "Gain Chosen");
+            } else {
+                loseOrGain = "lose";
+                Log.d(LOG_TAG, "Lose Chosen");
+            }
+        });
 
 
         final Button suggestionButton = findViewById(R.id.suggestionButton);
