d02c77862f873b1a707ed5742c49293c459c54dd
mukundm2 <mmukund@gmail.com>
2018-12-11 03:02:16 -0600

    Merge branch 'master' of https://github.com/sdhage2/HealthApp into Final_proj_muk
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/shiv/healthapp/activity_2.java
+++ app/src/main/java/com/example/shiv/healthapp/activity_2.java
@@ -2,6 +2,8 @@ package com.example.shiv.healthapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class activity_2 extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class activity_2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        Spinner mySpinner = (Spinner) findViewById(R.id.lose_gain);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(activity_2.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.loseORgain));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
+
+        Spinner mySpinner2 = (Spinner) findViewById(R.id.weighttolose);
+        ArrayAdapter<String> myAdapter2 = new ArrayAdapter<String>(activity_2.this, android.R.layout.simple_list_item_2, getResources().getStringArray(R.array.weighttolose));
+        myAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner2.setAdapter(myAdapter2);
+
     }
 }
--- app/src/main/java/com/example/shiv/healthapp/activity_3.java
+++ app/src/main/java/com/example/shiv/healthapp/activity_3.java
@@ -2,6 +2,7 @@ package com.example.shiv.healthapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class activity_3 extends AppCompatActivity {
 
